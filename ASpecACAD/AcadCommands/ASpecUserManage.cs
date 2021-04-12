7939cce70aec0574f3894b7353a6c28cc5e3b8f9
Aleksey Kulik <kpblc2000@gmail.com>
2021-04-12 16:09:15 +0300

    Вызов окна из-под ACAD

---
+++ ASpecACAD/AcadCommands/ASpecUserManage.cs
@@ -0,0 +1,21 @@
+﻿using ASpecCore.Views;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASpecACAD.AcadCommands
+{
+    public class ASpecUserManage
+    {
+        [CommandMethod("ASpecUserManage")]
+        public static void AcadCommandUserView()
+        {
+            UserView win = new UserView();
+            Application.ShowModalWindow(win);
+        }
+    }
+}
---
+++ ASpecACAD/AcadLisp/ASpecUserManage.cs
@@ -0,0 +1,22 @@
+﻿using ASpecCore.Views;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASpecACAD.AcadLisp
+{
+   public class ASpecUserManage
+    {
+        [LispFunction("ASpecUserManage")]
+        public static void AcadLispUserView(ResultBuffer args)
+        {
+            UserView win = new UserView();
+            Application.ShowModalWindow(win);
+        }
+    }
+}
