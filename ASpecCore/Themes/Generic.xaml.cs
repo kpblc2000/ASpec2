558d02ea5cc7da54786f3722e7784757b477e1cb
Aleksey Kulik <kpblc2000@gmail.com>
2021-04-13 14:59:05 +0300

    Подключение темы оформления. Настройка вида кнопок

---
+++ ASpecCore/Themes/Generic.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ASpecCore.Themes
+{
+    partial class Generic : ResourceDictionary
+    {
+        public Generic()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
