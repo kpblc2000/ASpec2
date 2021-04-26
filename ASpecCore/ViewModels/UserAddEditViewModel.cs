016b4b3643f4950a47632ee6ce732d7838036406
Aleksey Kulik <kpblc2000@gmail.com>
2021-04-26 19:27:55 +0300

    Ввод ViewModel для AddEditUser

---
+++ ASpecCore/ViewModels/UserAddEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ASpecCore.Models;
+using ASpecCore.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASpecCore.ViewModels
+{
+    internal class UserAddEditViewModel : ViewModel
+    {
+        private User _User;
+
+        public User User
+        {
+            get
+            {
+                return _User;
+            }
+            set
+            {
+                Set(ref _User, value, nameof(User));
+            }
+        }
+    }
+}
