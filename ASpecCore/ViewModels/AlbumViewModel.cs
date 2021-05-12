7e58b73f969e74609a608fb0ffadfe703987fadc
Aleksey Kulik <kpblc2000@gmail.com>
2021-05-12 21:25:47 +0300

    Проверка показа альбомов. Без настройки DataGrid

---
+++ ASpecCore/ViewModels/AlbumViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ASpecCore.Models.Data;
+using ASpecCore.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASpecCore.ViewModels
+{
+    public class AlbumViewModel : ViewModel
+    {
+        public AlbumViewModel()
+        {
+            Title = "Альбомы";
+            using (NPConDbFirstContext db = new NPConDbFirstContext())
+            {
+                Albums = new List<album>(db.albums);
+            }
+        }
+
+        public List<album> Albums
+        {
+            get { return _Albums; }
+            set { Set(ref _Albums, value); }
+        }
+
+        private List<album> _Albums;
+    }
+}
