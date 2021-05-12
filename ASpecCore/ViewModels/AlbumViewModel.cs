00a91809b361d730b6c1727a6857bdb138f5be8c
Aleksey Kulik <kpblc2000@gmail.com>
2021-05-12 16:11:13 +0300

    Выбор альбомов по критериям

--- ASpecCore/ViewModels/AlbumViewModel.cs
+++ ASpecCore/ViewModels/AlbumViewModel.cs
@@ -17,17 +17,102 @@ namespace ASpecCore.ViewModels
 
             using (NPConEntities db = new NPConEntities())
             {
-                Albums = new ObservableCollection<album>(db.albums);
+                _Albums = new List<album>(db.albums);
+                ShownAlbums = _Albums;
             }
         }
 
-        public ObservableCollection<album> Albums
+        public List<album> ShownAlbums
         {
-            get { return _Albums; }
-            set { Set(ref _Albums, value); }
+            get { return _ShownAlbums; }
+            set { Set(ref _ShownAlbums, value); }
         }
 
-        private ObservableCollection<album> _Albums;
+        public bool? ShowOnlyEndProductAlbums
+        {
+            get { return _ShowOnlyEndProductAlbums; }
+            set
+            {
+                if (Set(ref _ShowOnlyEndProductAlbums, value))
+                {
+                    if (value == null)
+                    {
+                        ShownAlbums = _Albums;
+                    }
+                    else
+                    {
+                        ShownAlbums = new List<album>(_Albums.Where(o => o.is_end_prod_alb == value));
+                    }
+                }
+            }
+        }
+
+        public bool? ShowOnlyFactoryAlbums
+        {
+            get { return _ShowOnlyFactoryAlbums; }
+            set
+            {
+                if (Set(ref _ShowOnlyFactoryAlbums, value))
+                {
+                    if (value == null)
+                    {
+                        ShownAlbums = _Albums;
+                    }
+                    else if (value == true)
+                    {
+                        ShownAlbums = new List<album>(_Albums.Where(o => o.factory != null));
+                    }
+                    else
+                    {
+                        ShownAlbums = new List<album>(_Albums.Where(o => o.factory == null));
+                    }
+                }
+            }
+        }
+
+        private void RefillShownAlbums()
+        {
+            List<album> resList = new List<album>();
+            if (_ShowOnlyEndProductAlbums == null && _ShowOnlyFactoryAlbums == null)
+            {
+                resList = _Albums;
+            }
+            else if (_ShowOnlyEndProductAlbums != null && _ShowOnlyFactoryAlbums == null)
+            {
+                foreach (album item in _Albums.Where(o => o.is_end_prod_alb == _ShowOnlyEndProductAlbums))
+                {
+                    resList.Add(item);
+                }
+            }
+            else if (_ShowOnlyEndProductAlbums != null && _ShowOnlyFactoryAlbums == true)
+            {
+                foreach (var item in _Albums.Where(o => o.is_end_prod_alb == _ShowOnlyEndProductAlbums && o.id_fact != null))
+                {
+                    resList.Add(item);
+                }
+            }
+            else if (_ShowOnlyEndProductAlbums != null && _ShowOnlyFactoryAlbums == false)
+            {
+                foreach (var item in _Albums.Where(o => o.is_end_prod_alb == _ShowOnlyEndProductAlbums && o.id_fact == null))
+                {
+                    resList.Add(item);
+                }
+            }
+            if (resList.Count==0)
+            {
+                ShownAlbums = new List<album>();
+            }
+            else
+            {
+                ShownAlbums = resList;
+            }
+        }
+
+        private List<album> _Albums;
+        private List<album> _ShownAlbums;
+
+        private bool? _ShowOnlyEndProductAlbums;
+        private bool? _ShowOnlyFactoryAlbums;
 
     }
 }
