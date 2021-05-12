dbd5c9a47db7af4db66a2c9e13ca7b2b45909060
Aleksey Kulik <kpblc2000@gmail.com>
2021-05-12 14:49:04 +0300

    Подключение к БД. Хоть что-то

---
+++ ASpecCore/Models/Data/CustomNPConDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASpecCore.Models.Data
+{
+    public partial class NPConEntities : DbContext
+    {
+        public NPConEntities(string ConnectionString) : base(ConnectionString) { }
+    }
+}
