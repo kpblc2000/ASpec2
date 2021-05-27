8deb64f69acd76c5507c94b8efb78561eff93179
Aleksey Kulik <kpblc2000@gmail.com>
2021-05-27 13:01:38 +0300

    Ввод команды закрытия активного окна

---
+++ ASpecCore/Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,29 @@
+﻿using ASpecCore.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ASpecCore.Infrastructure.Commands
+{
+    class CloseWindowCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            var win = parameter as Window;
+            if (win == null)
+            {
+                win = Application.Current.Windows.Cast<Window>().FirstOrDefault(i => i.IsFocused);
+            }
+            if (win == null)
+            {
+                win = Application.Current.Windows.Cast<Window>().FirstOrDefault(i => i.IsActive);
+            }
+            win?.Close();
+        }
+    }
+}
