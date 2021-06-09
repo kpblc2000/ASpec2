9d6ecc5c48e30164b08a314941e0f77d817cae6a
Aleksey Kulik <kpblc2000@gmail.com>
2021-06-09 17:57:27 +0300

    Можо вводить и целые значения

--- ASpecCore/Infrastructure/Enums/SqlUserRole.cs
+++ ASpecCore/Infrastructure/Enums/SqlUserRole.cs
@@ -5,13 +5,13 @@ namespace ASpecCore.Infrastructure.Enums
     [Flags]
     public enum SqlUserRole
     {
-        Unknown         = 0b_0000_0000,
-        ReadWrite       = 0b_0000_0001,
-        ReadOnly        = 0b_0000_0010,
-        DesignTeam      = 0b_0000_0100,
-        ConfirmElements = 0b_0000_1000,
-        AlbumAdmin      = 0b_0001_0000,
-        EndProdAdmin    = 0b_0010_0000,
-        NetDeveloper    = 0b_0100_0000
+        Unknown         = 0,
+        ReadWrite       = 1,
+        ReadOnly        = 2,
+        DesignTeam      = 4,
+        ConfirmElements = 8,
+        AlbumAdmin      = 16,
+        EndProdAdmin    = 32,
+        NetDeveloper    = 64
     }
 }
