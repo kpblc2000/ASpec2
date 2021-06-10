fb774dd4b4e8635492f7babdb681930d3d0c1272
Aleksey Kulik <kpblc2000@gmail.com>
2021-06-10 14:58:00 +0300

    Ввод новой ViewModel и View для списка продукции

---
+++ ASpecCore/ViewModels/EndProductViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ASpecCore.Models.Data;
+using ASpecCore.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASpecCore.ViewModels
+{
+    public class EndProductViewModel : ViewModel
+    {
+        public EndProductViewModel()
+        {
+            using (NPConDataModel db = new NPConDataModel())
+            {
+                EndProducts = new ObservableCollection<view_endproduct_cut>(db.view_endproduct_cut);
+            }
+        }
+
+        public ObservableCollection<view_endproduct_cut> EndProducts
+        {
+            get { return _EndProducts; }
+            set { Set(ref _EndProducts, value); }
+        }
+
+        private ObservableCollection<view_endproduct_cut> _EndProducts;
+
+    }
+}
