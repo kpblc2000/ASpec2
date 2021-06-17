63cc1e68cd0d4708560f6f1d123f3b3e8cb34460
Aleksey Kulik <kpblc2000@gmail.com>
2021-06-17 12:17:47 +0300

    Спер код по клонированию объектов с достаточно слабым пониманием, как оно работает и почему

---
+++ ASpecCore/Infrastructure/ObjectExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASpecCore.Infrastructure
+{
+    public static class ObjectExtensions
+    {
+        public static T Clone<T>(this T entity) where T : class
+        {
+            var clone = Activator.CreateInstance(entity.GetType());
+            var entPropValueDict = entity.AsPropValueDictionary();
+            foreach (var prop in clone.GetType().GetProperties())
+            {
+                clone.GetType().GetProperty(prop.Name).SetValue(clone, entPropValueDict[prop.Name]);
+            }
+            return clone as T;
+        }
+
+        public static IDictionary<string, object> AsPropValueDictionary<T>(this T instance, params BindingFlags[] bindingFlags)
+        {
+            var runtimeBindFlags = BindingFlags.Default;
+            if (bindingFlags.Count() == 0)
+            {
+                runtimeBindFlags = BindingFlags.Default;
+            }
+            else if (bindingFlags.Count() == 1)
+            {
+                runtimeBindFlags = bindingFlags[0];
+            }
+            else
+            {
+                runtimeBindFlags = bindingFlags.Aggregate(runtimeBindFlags, (current, bindingFlag) => current | bindingFlag);
+            }
+
+            return runtimeBindFlags == BindingFlags.Default
+                ? instance.GetType().GetProperties().ToDictionary(prop => prop.Name, prop => prop.GetValue(instance))
+                : instance.GetType().GetProperties(runtimeBindFlags).ToDictionary(prop => prop.Name, prop => prop.GetValue(instance));
+
+        }
+    }
+
+}
