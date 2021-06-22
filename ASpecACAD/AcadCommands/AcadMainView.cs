af97db686ee132bc4595a21d2ef15d59a72cbb77
Aleksey Kulik <kpblc2000@gmail.com>
2021-06-22 18:56:17 +0300

    Прописаны команды для работы в ACAD. Поставлены "заглушки"

---
+++ ASpecACAD/AcadCommands/AcadMainView.cs
@@ -0,0 +1,42 @@
+﻿using ASpecACAD.Models.DwgData;
+using ASpecCore.Models.Data;
+using ASpecCore.ViewModels;
+using ASpecCore.Views;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASpecACAD.AcadCommands
+{
+    public class AcadMainView
+    {
+        [CommandMethod("RunMain")]
+        public static void AcadMainView_RunMainCommand()
+        {
+            AcadMainViewCommand();
+        }
+
+        [CommandMethod("RunMo")]
+        public static void AcadMainView_RunMainObjectCommand()
+        {
+            AcadMainViewCommand();
+        }
+
+        private static void AcadMainViewCommand()
+        {
+            ASpecMainView winASpecMain = new ASpecMainView();
+            ASpecMainViewModel winVM = winASpecMain.DataContext as ASpecMainViewModel;
+            winVM.EndProdVM.RegisterMatchToCurrentDwgHadler(MatchEndProdIdToDwg);
+            winASpecMain.ShowDialog();
+        }
+
+        private static bool MatchEndProdIdToDwg(view_endproduct_cut CurrentEndProduct)
+        {
+            EndProduct temp = new EndProduct();
+            return temp.Save(CurrentEndProduct.id_endprod_ver.ToString());
+        }
+    }
+}
---
+++ ASpecACAD/Models/DwgData/EndProduct.cs
@@ -0,0 +1,19 @@
+﻿using ASpecACAD.Interfaces;
+using Autodesk.AutoCAD.ApplicationServices;
+using System;
+
+namespace ASpecACAD.Models.DwgData
+{
+    class EndProduct : IDwgData
+    {
+        public string Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(string Data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
